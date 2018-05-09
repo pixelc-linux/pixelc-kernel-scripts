5b67d363213f25d8d769337f24feba2f1cc64c5a
q66 <daniel@octaforge.org>
2018-05-09 10:51:21 +0200

    add missing quote

--- config.sh
+++ config.sh
@@ -12,7 +12,7 @@ fi
 
 if [ "$KERN_CONFIGTOOL" = "defconfig" ] && [ -z "$KERN_BUILD_IN_TREE" ]; then
     rm -rf "$KERN_BUILDDIR"
-    mkdir -p "$KERN_BUILDDIR
+    mkdir -p "$KERN_BUILDDIR"
 fi
 
 make ARCH=arm64 CROSS_COMPILE="$KERN_CROSS" \
