d2bbf44eaced2956642f64c70bd8dd08266e5338
q66 <daniel@octaforge.org>
2018-05-09 10:49:55 +0200

    fix builddir being recreated

--- config.sh
+++ config.sh
@@ -10,5 +10,10 @@ if [ -n "$KERN_CONFIGTOOL" ]; then
     CONFIGTOOL="$KERN_CONFIGTOOL"
 fi
 
+if [ "$KERN_CONFIGTOOL" = "defconfig" ] && [ -z "$KERN_BUILD_IN_TREE" ]; then
+    rm -rf "$KERN_BUILDDIR"
+    mkdir -p "$KERN_BUILDDIR
+fi
+
 make ARCH=arm64 CROSS_COMPILE="$KERN_CROSS" \
     KBUILD_DEFCONFIG="$KERN_DEFCONFIG" "$CONFIGTOOL" O="$KERN_BDIR" "$@"
